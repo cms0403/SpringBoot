64e8f0ccedbe5a9baadc4b7c6f228629b96a7bd8
SataNVme <kimyc8958@gmail.com>
2022-04-13 09:13:58 +0900

    commit

---
+++ src/test/java/com/proejct2/movieproject/MovieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proejct2.movieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
