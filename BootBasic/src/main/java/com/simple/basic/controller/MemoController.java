948d543db798aaad56b7b9727d7fbde14192b123
cms0403 <cmsjy040317@naver.com>
2022-04-17 19:40:08 +0900

    SpringBoot study

---
+++ BootBasic/src/main/java/com/simple/basic/controller/MemoController.java
@@ -0,0 +1,83 @@
+package com.simple.basic.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.simple.basic.command.MemoVO;
+import com.simple.basic.memo.MemoService;
+
+@Controller
+@RequestMapping("/memo")
+public class MemoController {
+	
+	@Autowired
+	@Qualifier("memoService")
+	private MemoService memoService;
+	
+	//예제코드 (컨트롤러-서비스-매퍼 연결)
+	@GetMapping("/ex")
+	public void ex(Model model) {
+		
+		String time = memoService.getTime();
+		
+		model.addAttribute("time", time);
+		
+	}
+	//화면처리
+	@GetMapping("/memoList")
+	public String memoList(Model model) {
+		
+		//select
+		ArrayList<MemoVO> list = memoService.getList();
+		model.addAttribute("list", list);
+		
+		return "memo/memoList";
+	}
+	
+	//화면처리
+	@GetMapping("/memoWrite")
+	public String memoWrite() {
+		
+		return "memo/memoWrite";
+	}
+	
+	//요청폼(등록)
+	@PostMapping("/memoForm")
+	public String memoForm(@Valid MemoVO vo, Errors errors, Model model) {
+		
+		//유효성검사
+		if(errors.hasErrors()) { //유효성 검사 실패시 true
+			List<FieldError> list = errors.getFieldErrors(); //유효성검사 실패목록
+			for(FieldError err : list) {
+				model.addAttribute("valid_" + err.getField() , err.getDefaultMessage() );
+			}
+			
+			model.addAttribute("vo", vo); //화면에 처리할 데이터
+			return "memo/memoWrite";
+		}
+		
+		//등록
+		memoService.memoInsert(vo);
+		
+		
+		return "redirect:/memo/memoList"; //리스트화면으로
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ BootBasic/src/main/java/com/simple/basic/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.simple.basic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/home")
+	public String test() {
+		return "home";
+	}
+	
+	
+	
+	@ResponseBody
+	@GetMapping("hello2")
+	public String hello2() {
+		
+		return "안녕하세요2";
+	}
+	
+	
+	
+	
+}
